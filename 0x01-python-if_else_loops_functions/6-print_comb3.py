490d0b5baad526314dbb627b84d81f2cab67d88b
PreciousEzeigbo <preciousezeigbo*1@gmail.com>
2023-06-06 13:29:26 -0700

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 #6-print_combo3.py
 
-#Print all possible different combinations of two digits in ascending order.
-#
-#The two digits must be different - 01 and 10 are considered identical.
+"""Print all possible different combinations of two digits in ascending order.
+
+The two digits must be different - 01 and 10 are considered identical.
+"""
 
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
