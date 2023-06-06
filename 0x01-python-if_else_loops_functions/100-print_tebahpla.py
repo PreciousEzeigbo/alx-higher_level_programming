2357a9730dc840933119c446828eb07139bfc29c
PreciousEzeigbo <preciousezeigbo*1@gmail.com>
2023-06-06 15:32:14 -0700

    Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # 100-print_tebahpla.py
+
 """Print the alphabet in reverse order alternating upper- and lower-case."""
+
 for ch in reversed(range(97, 123)):
     print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end= '')
