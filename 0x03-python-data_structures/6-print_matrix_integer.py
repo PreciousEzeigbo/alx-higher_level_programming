44c8cac7b2172022bae2e1c8175ecbb70481c7e9
PreciousEzeigbo <preciousezeigbo*1@gmail.com>
2023-06-12 12:41:49 -0700

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for elen in matrix:
+            i = 1
+            length = len(elen)
+            for elens in elen:
+                if i == length:
+                    print('{:d}'.format(elens), end='')
+                else:
+                    print('{:d}'.format(elem), end= ' ')
+                    i += 1
+            print()
