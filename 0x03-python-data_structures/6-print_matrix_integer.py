779b3bae5c6768ec6a7e0637894bed8f05db9986
PreciousEzeigbo <preciousezeigbo*1@gmail.com>
2023-06-12 12:46:42 -0700

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        for elen in matrix:
+        for elems in matrix:
             i = 1
-            length = len(elen)
-            for elens in elen:
+            length = len(elems)
+            for elem in elems:
                 if i == length:
-                    print('{:d}'.format(elens), end='')
+                    print('{:d}'.format(elem), end='')
                 else:
-                    print('{:d}'.format(elens), end= ' ')
-                    i += 1
+                    print('{:d}'.format(elem), end= ' ')
+                i += 1
             print()
