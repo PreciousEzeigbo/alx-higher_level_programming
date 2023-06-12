99f1c0e86d64b899af10047caec37762a196e5f7
PreciousEzeigbo <preciousezeigbo*1@gmail.com>
2023-06-12 13:18:21 -0700

    Task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=())
-first_element = tuple_a + (0, 0)
-second_element = tuple_b + (0, 0)
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = validate_tuple(tuple_a)
+    tuple_b = validate_tuple(tuple_b)
 
-result1 = 0
-result2 = 0
+    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
 
-result1 += first_element[0] + second_element[0]
-result2 += first_element[1] + second_element[1]
+def validate_tuple(_tuple=()):
+    if len(_tuple) < 2:
+        if len(_tuple) == 1:
+            _tuple = (_tuple[0], 0)
+        elif len(_tuple) == 0:
+            _tuple = (0, 0)
+    elif len(_tuple) > 2:
+        _tuple = (_tuple[0], _tuple[1])
 
-return (result1, result2)
+        return_tuple
