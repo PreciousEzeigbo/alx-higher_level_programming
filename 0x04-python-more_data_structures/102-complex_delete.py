b58367068a0915189be366c26c2442ef8fd9889e
PreciousEzeigbo <preciousezeigbo*1@gmail.com>
2023-06-14 14:01:56 -0700

    Task 15

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def comple_delete(a_dictionary, value):
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[keys] == value:
+            del a_dictionary[key]
+    return a_dictionary
