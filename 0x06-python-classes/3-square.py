5374f21fa7d433d44bb1122ba7b463701685c9a0
PreciousEzeigbo <preciousezeigbo*1@gmail.com>
2023-06-28 00:21:28 -0700

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
+
 """ class Square that defines a square"""
 
 
 class Square:
-    """ class Square that defines a square"""
-
+     """
+    square class with a private attribute size
+    and checks data type
+    """
     def __init__(self, size=0):
-         """initialize square
-        Args:
-            size (int): size of the square
-        """
         if type(size) is not int:
             raise TypeError('size must be an integer')
-        elif size < 0:
+        if size < 0:
             raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
+
+        self.__size = size
 
     def area(self):
-         """returns the area.
-
-        Returns:
-            ares.
-        """
+         """returns the current area"""
         return self.__size**2
