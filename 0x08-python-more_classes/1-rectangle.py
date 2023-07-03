12deaf6173df9fa0be062266f0a62610231ec623
PreciousEzeigbo <preciousezeigbo*1@gmail.com>
2023-07-03 12:22:27 -0700

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,7 +31,6 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-
         self.__width = value
 
     @height.setter
@@ -42,5 +41,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >=0")
-
         self.__height = value
