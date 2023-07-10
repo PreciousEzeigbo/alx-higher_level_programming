ceb3574856670fd0dec4b1171e65f72c78b96e6d
PreciousEzeigbo <preciousezeigbo*1@gmail.com>
2023-07-10 14:10:19 -0700

    Task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-""" module subclass rectangle
-"""
+
+"""The module is 8-base_geometry"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle that inherits from BaseGeometry"""
+    """A subclass of BaseGeometry class"""
+
     def __init__(self, width, height):
-        BaseGeometry.integer_validator(self, 'height', height)
-        self.__height = height
-        BaseGeometry.integer_validator(self, 'width', width)
+        """initialize private attributes"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
+        self.__height = height
