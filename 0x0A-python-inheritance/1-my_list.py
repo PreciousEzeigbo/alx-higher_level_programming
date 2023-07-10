22d782ca718e489901adcc53e313bb20708355a1
PreciousEzeigbo <preciousezeigbo*1@gmail.com>
2023-07-10 13:26:30 -0700

    Task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,10 @@
 
 class MyList(list):
     """inherits from list"""
+    def __init__(self):
+        """initilize the object"""
+        super().__init__()
+
     def print_sorted(self):
         """prints the list, but sorted
         (ascending sort)
