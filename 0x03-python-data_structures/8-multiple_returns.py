0b41aa6cbf3adc4e06afb97d46953b33d16bb8b0
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-03-18 09:49:06 -0700

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,5 @@
 def multiple_returns(sentence):
     if not sentence:
         return (0, None)
-
-    len(sentence) = length
-    first_character = sentence[0]
-    return length, first_character
+    else:
+        return (len(sentence), sentence[0])
