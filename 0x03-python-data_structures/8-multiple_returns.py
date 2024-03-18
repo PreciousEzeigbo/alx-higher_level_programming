c11279a6f62f21404e723aab891fba68ca6b87b0
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-03-18 09:46:04 -0700

    Task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        return (0, None)
+
+    len(sentence) = length
+    first_character = sentence[0]
+    return length, first_character
