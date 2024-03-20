b7f43c98ce8b643b4aed9a0917d8ba6e57bef7a2
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-03-20 08:24:52 -0700

    Task 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    keys = sorted(a_dictionary.keys())
+    for i in keys:
+        value = a_dictionary[key]
+    return (f"{key}: {value}")
