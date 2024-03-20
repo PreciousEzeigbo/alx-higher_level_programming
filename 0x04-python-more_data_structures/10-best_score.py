c4151858025a015af2effc372bab9d5f54a37f25
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-03-20 09:33:44 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    #initialize variable to track maximum score
-    i = 0
-    keys = None
-
-    #iterate through the dictionary
-    for key,value in a_dictionary.items():
-        if value > i:
-            i = value
+    if not a_dictionary:
+        return (None)
 
     #returns the key with the highest value
-    return keys
+    return (max(a_dictionary, key=a_dictionary.get)
