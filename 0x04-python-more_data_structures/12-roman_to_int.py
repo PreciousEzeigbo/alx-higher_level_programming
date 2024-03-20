ec155d7d19fc7e3a6190d384214d2e4437c8f414
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-03-20 10:15:00 -0700

    Task 12 Technical interview preparation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,41 @@
 #!/usr/bin/python3
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
+
+
 def roman_to_int(roman_string):
-    if type(roman_string) is not  str or roman_string is None:
+    if not roman_string:
         return 0
 
-    roman_numerals = {
-        ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
-        ['X', 10], ['V', 5], ['I', 1]
-    }
-    total = 0
-    prev_value = 0
+    if not isinstance(roman_string, str):
+        return 0
 
-    for symbol in roman_string:
-        for element in roman_numerals:
-            if symbol == element[0]:
-                if prev_value == 0 or prev_value >= element[1]:
-                    total += element[1]
-                elif prev_value < element[1]:
-                    total += element[1] - (prev_value * 2)
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
 
-                prev_value = element[1]
+    num = 0
+    last_rom = 0
+    list_num = [0]
 
-    return total
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
+                else:
+                    list_num.append(rom_n.get(ch))
+
+                last_rom = rom_n.get(ch)
+
+    num += to_subtract(list_num)
+
+    return (num)
 
