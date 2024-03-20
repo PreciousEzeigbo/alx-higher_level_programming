bdfdfb56c142ca170adcb0dcb6a60a746878295a
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-03-20 07:36:26 -0700

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = list(my_list)
     for element in range(len(new_list)):
-        if element == search:
-            element = replace
+        if new_list[element] == search:
+            new_list[element] = replace
     return new_list
