1324d35d897ab22104e7a8e7cf5cb46ca7e806ce
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-04-07 15:50:37 -0700

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,12 @@
 class Square:
     """Class square that defines a square"""
     def __init__(self, size):
-        """Public instance attribute"""
-        self.size = size  #: size of square
+        """__init__
+
+        The __init__ method initializes the size value
+        of the square.
+
+        Attributes:
+            size (int): The size of the square.
+        """
+        self.__size = size
