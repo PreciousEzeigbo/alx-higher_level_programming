1a46df1737d7c34c5529f3681be71e3885c358a4
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-04-07 15:45:18 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A Class that defines a square"""
+
+
+class Square:
+    """Class square that defines a square"""
+    def __init__(self, size):
+        """Public instance attribute"""
+        self.size = size  #: size of square
