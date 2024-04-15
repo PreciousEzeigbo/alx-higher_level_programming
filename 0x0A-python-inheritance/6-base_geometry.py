61f59c762fb2a7ac251c16256269d6df43ad160e
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-04-15 07:40:11 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a class based off 5-base_geometry.py"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def area(self):
+        rasie Exception("area() is not implemented")
