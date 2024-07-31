4a43a7a1be22cbd1954dc4b7fcc76e1bade22a86
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-07-31 15:38:47 -0700

    Advanced Task

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,24 +1,21 @@
 #!/usr/bin/node
 
-const axios = require('axios');
-
+const req = require('request');
 const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${id}/`;
-
-async function getCharacterNames() {
-  try {
-    // Fetch film data
-    const filmResponse = await axios.get(url);
-    const characters = filmResponse.data.characters;
-
-    // Fetch and log character names
-    for (const characterUrl of characters) {
-      const characterResponse = await axios.get(characterUrl);
-      console.log(characterResponse.data.name);
-    }
-  } catch (error) {
-    console.error('Error:', error.message);
+const url = 'https://swapi-api.hbtn.io/api/films/';
+req.get(url + id, function (error, res, body) {
+  if (error) {
+    console.log(error);
   }
-}
-
-getCharacterNames();
+  const data = JSON.parse(body);
+  const dd = data.characters;
+  for (const i of dd) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
+        console.log(error);
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
+});
