73d89f164e38c3286b9397819f4b020bf2148ea5
PreciousEzeigbo <preciousezeigbo@gmail.com>
2024-08-16 18:16:23 -0700

    fix

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -7,7 +7,7 @@ const firstArg = args[0]; // gets first argument
 
 const secondArg = args[1]; // gets second argument
 
-/*if (firstArg === undefined || secondArg === undefined) {
+/* if (firstArg === undefined || secondArg === undefined) {
   console.log(firstArg + ' is ' + secondArg);
 }
 */
